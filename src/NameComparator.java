64d5be93dcd2a8163b840a81cae50d453f9b3ad3
MatiuninaMarina <aprelkina.marina@mail.ru>
2023-03-11 19:01:33 +0400

    Code has been reformated

--- src/NameComparator.java
+++ src/NameComparator.java
@@ -6,11 +6,12 @@ public class NameComparator implements Comparator<Person> {
     public NameComparator(int maxWords) {
         this.maxWords = maxWords;
     }
+
     @Override
     public int compare(Person o1, Person o2) {
         int length1 = o1.getSurname().split(" ").length;
         int length2 = o2.getSurname().split(" ").length;
-        boolean longSurnames = length1>=maxWords && length2>=maxWords;
+        boolean longSurnames = length1 >= maxWords && length2 >= maxWords;
         if (length2 == length1) {
             if (o1.getAge() > o2.getAge()) {
                 return 1;
@@ -22,7 +23,7 @@ public class NameComparator implements Comparator<Person> {
 
         } else if (length2 > length1 && !longSurnames) {
             return -1;
-        } else if  ((length2 > length1 || length1>length2) && longSurnames) {
+        } else if ((length2 > length1 || length1 > length2) && longSurnames) {
             if (o1.getAge() > o2.getAge()) {
                 return 1;
             } else if (o2.getAge() > o1.getAge()) {

6da250351730253316149e41f35f1192d43c6a3c
MatiuninaMarina <aprelkina.marina@mail.ru>
2023-03-16 20:17:34 +0400

    Added branch eraser

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        Person person2 = new Person("Vika", "Litvinenko", 9);
+        Person person2 = new Person("Vika", "Litvinenko", 19);
         Person person = new Person("Zhenya", "Voronina", 15);
         Person person1 = new Person("Alisa", "Avdeeva Aprelkina", 5);
-        Person person3= new Person("Marina", "Voronina-Sokolova-Matyunina", 17);
+        Person person3 = new Person("Marina", "Voronina-Sokolova-Matyunina", 17);
         Person person4 = new Person("Nadezhda", "Vihanova-Ivanova-Petrova-Seleznyova", 18);
-        Person person5 = new Person("Vera", "Denisov Ivanova Petrova Seleznyova Strelcova", 4);
+        Person person5 = new Person("Vera", "Denisov Ivanova Petrova Seleznyova Strelcova", 24);
         ArrayList<Person> listOfPeople = new ArrayList<>();
         listOfPeople.add(person3);
         listOfPeople.add(person4);
@@ -16,6 +17,8 @@ public class Main {
         listOfPeople.add(person);
         listOfPeople.add(person1);
         listOfPeople.add(person2);
+        Predicate<Person> personPredicate = p -> p.getAge() < 18;
+        listOfPeople.removeIf(personPredicate);
         Collections.sort(listOfPeople, new NameComparator(3));
         System.out.println(listOfPeople);
         System.out.println();
